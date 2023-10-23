bf31786a5b88ac3c6913bfe60967fc36a555892e
FullstackRakibu <rakibul.cse.47@gmail.com>
2023-10-23 17:37:55 +0600

    dependency error

---
+++ CleanMovie.API/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanMovie.API.Controllers
+{
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _service;
+
+        public MoviesController (IMovieService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+
+        public ActionResult <List<Movie>> Get()
+        {
+            var moviesFormService = _service.GetAllMovies();
+            return Ok(moviesFormService);
+        }
+    }
+}
---
+++ CleanMovie.Infrastructure/MovieRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Infrastructure
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public static List<Movie> movies = new List<Movie>() {
+            new Movie { Id = 1, Name = "God Father" , Cost = 550.0M },
+            new Movie { Id = 1, Name = "Whisper of the Heart" , Cost = 550.0M },
+            new Movie { Id = 1, Name = "12 Angry Men" , Cost = 550.0M },
+        };
+        public List<Movie> GetAllMovies()
+        {
+            return movies;
+        }
+    }
+}
